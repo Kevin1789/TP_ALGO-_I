73349b4ae0c2206d65fbe1d9c71012e4a0c29ffb
Kevin1789 <krojas@fi.uba.ar>
2023-05-05 20:24:40 -0300

    commit -m "Unidad 1" [main 5f0b5a0] Unidad 1  1 file changed, 1 insertion(+), 1 deletion(-) ``` ``` $ git push origin main

--- Unidad_1.py
+++ Unidad_1.py
@@ -19,9 +19,54 @@ carácter especial, y que sea de la longitud correcta para el turno.
 def palabras_a_jugar ():
     #import unidad 3
     #palabras_elegidas = [[1, "entropecer", "info"],[2, "unicornio_paranoico", "uni"],[3,"Krussu", "info"]]
-    palabras_elegidas = {1: ["cuarteto de nos", "la mejor banda de rock papa"], 2: ["unicornio_paranoico", "uni"], 3:["krussu", "yo"]}
+    palabras_elegidas = {1: ["cuarteto de nos", "la mejor banda de rock papa"], 2: ["unicornio_paranoico", "uni"], 3:["kryssu", "yo"]}
     #print(palabras_elegidas)
     return palabras_elegidas
+
+def letras_palabras(palabras_elegidas):
+    letras = []
+    palabras = []
+    claves = []
+    for clave, valor in palabras_elegidas.items():
+        letras.append(valor[0][0])
+        palabras.append(valor[0])
+        claves.append(clave)
+    #print(letras)
+    #print(palabras)
+    #print(claves)
+    return letras, palabras, claves
+
+#letras_palabras(palabras_a_jugar())
+
+j1 = letras_palabras(palabras_a_jugar())[0][0]
+j2 = letras_palabras(palabras_a_jugar())[0][1]
+j3 = letras_palabras(palabras_a_jugar())[0][2]
+
+def descripcion(palabras_elegidas):
+    for clave, valor in palabras_elegidas.items():
+        valor[1]
+        #print (valor[1])
+    return valor[1]
+
+
+d1 = descripcion(palabras_a_jugar())
+
+
+
+def turno_letra (letras):
+    for inicial in letras:
+        inicial
+        #print(inicial)
+    return inicial
+
+i1 = turno_letra(letras_palabras(palabras_a_jugar())[0][0])
+
+
+
+
+#turno_letra(letras_palabras(palabras_a_jugar())[0])
+
+
 
 def juego (palabras_elegidas):
     for clave, valor in palabras_elegidas.items():
@@ -32,10 +77,50 @@ def juego (palabras_elegidas):
             print("correcto")
         else:
             print("incorrecto")
-        
+    return palabra, valor[0][0], valor[1], clave #palabra, letra, definicion, clave
 
-juego(palabras_a_jugar())
-    
+#juego(palabras_a_jugar())
+
+
+def juego_ok(palabra_elegidas):
+    dicc = palabra_elegidas
+    n1 = " "
+    aciertos = 0
+    errores = 0
+    j1 = letras_palabras(palabras_a_jugar())[0][0]
+    turno_letra = letras_palabras(palabras_a_jugar())[0][0]
+    longitud_palabra = len(palabra_elegidas[1][0])
+    definicion = palabra_elegidas[1][1]
+    tablero
+    print(f"""
+[{j1}][2][3][4][5][6][7][8][9][0]
+[{n1}][2][3][4][5][6][7][8][9][0]
+Aciertos: {aciertos}
+Errores: {errores}
+Turno letra {turno_letra} - palabra de {longitud_palabra} letras
+Definicion: {definicion}""")
+    palabra = input()
+    for clave, valor in palabra_elegidas.items():
+            if palabra == valor[0]:
+                print("correcto")
+                n1 = "a"
+                aciertos = aciertos + 1
+            else:
+                print("incorrecto")
+                n1 = "e"
+                errores = errores + 1
+            
+
+    return palabra, valor[0][0], valor[1], clave #palabra, letra, definicion, clave
+
+juego_ok(palabras_a_jugar())
+
+            
+
+
+#juego_ok(palabras_a_jugar())
+
+
 
 #funciones para cada juego
 def juego_1 (palabras_elegidas):
@@ -65,25 +150,19 @@ def juego_3 (palabras_elegidas):
 
 #juego_3(palabras_a_jugar())
 
-def mostrar_tablero(juego_1, juego_2, juego_3):
-    j1 = juego_1[2]
-    #print(j1)
-    j2 = juego_2[2]
-    #print(j2)
-    j3 = juego_3[2]
-    #print(j3)
-    return j1, j2, j3
 
 #mostrar_tablero(juego_1(palabras_a_jugar()), juego_2(palabras_a_jugar()), juego_3(palabras_a_jugar()))
 
 #pruebas
+
 j1 = juego_1(palabras_a_jugar())[2]
 j2 = juego_2(palabras_a_jugar())[2]
 j3 = juego_3(palabras_a_jugar())[2]
 d1 = juego_1(palabras_a_jugar())[1]
 d2 = juego_2(palabras_a_jugar())[1]
 d3 = juego_3(palabras_a_jugar())[1]
-n1 = " "
+n1 = ""
+
 
 #funsion que permite ver los errores y aciertos
 def aciertos_errores ():
@@ -137,8 +216,7 @@ def tablero ():
 
 Aciertos: {aciertos_errores()[0]}
 Errores: {aciertos_errores()[1]}
-Turno letra [?] - palabra de {l_palabra} letras
+Turno letra {i1} - palabra de {l_palabra} letras
 Definicion: {d1}""")
 #tablero()
 #segunda_linea(ingreso_palabra(juego_1(palabras_a_jugar()), aciertos_errores()[0], aciertos_errores()[1])[3], n1)
-
