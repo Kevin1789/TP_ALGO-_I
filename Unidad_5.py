96bbeb64732645d4e6fcfef44d1d4f031bd8094e
Kevin1789 <krojas@fi.uba.ar>
2023-05-07 03:52:55 -0300

    La sufrida v0.4.0

---
+++ Unidad_5.py
@@ -0,0 +1,12 @@
+"""
+Etapa 5 - Puntaje
+Hasta ahora nuestro jugador, simplemente gana o pierde.
+En esta etapa vamos a permitir que obtenga puntaje y que el mismo se acumule de
+partida tras partida.
+Por cada palabra correcta el usuario suma 10 puntos, por cada palabra incorrecta el
+jugador pierde 3 puntos. El puntaje final obtenido podrá ser negativo.
+Al finalizar una partida, se le ofrecerá si desea jugar otra; así hasta que responda que
+no. El puntaje obtenido en la última partida, se tomará como inicio de la siguiente. Al
+inicio de la ejecución del 1er. juego, el puntaje se encuentra en cero.
+"""
+
