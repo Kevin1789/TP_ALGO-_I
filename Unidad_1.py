3369086e54c9e09b45dd412d06808e08c836b42c
Kevin1789 <krojas@fi.uba.ar>
2023-05-19 16:08:33 -0300

    La Sufrida v0.5.0

--- Unidad_1.py
+++ Unidad_1.py
@@ -18,111 +18,104 @@ carácter especial, y que sea de la longitud correcta para el turno.
 
 #ayuddaaaaa
 #no se que estoy haciendo TT
+dicc = {"casa": "lugar donde vives", "perro": "animal domestico", "gato": "animal domestico"}
 
-dicc = {"casa":"lugar donde vives", "perro":"animal domestico", "gato":"animal domestico"}
-aciertos = 0
-errores = 0
-clave = ""
-valor = ""
-def palabra (dicc):
-    for clave, valor in dicc.items():
-        palabra = clave
-    return palabra
-
-def aciertos_errores (palabra, clave):
-    if palabra == clave:
-        aciertos = aciertos + 1
-        print("aciertos:", aciertos)
-    elif palabra != clave :
-        errores = errores + 1
-        print("errores:", errores)
-    return aciertos, errores
-
-longitud = 0
-
-def longotud_palabra (palabra):
-    longitud = len(palabra)
-    return longitud
-
-definicion = ""
-
-def definicion_palabra (valor):
-    definicion = valor
-    return definicion
-
-lista = [" "," "," "]
-
-def mostrar_tablero (dicc):
-    for letra, clave in enumerate(dicc.keys()):
-        lista[letra] = clave[0]
-    print(f"""[{lista[0]}][{lista[1]}][{lista[2]}]""")
-#mostrar_tablero(dicc)
-
-
-lista_2 = []
-
-def agregar_lista (lista_2, aciertos):
-    for aciertos in lista_2:
-        if aciertos + 1:
-            lista_2.append("a")
+def agregar_lista(lista_2,aciertos,errores):
+    #aciertos, errores
+    for i in range(len(lista_2)):
+        if i < aciertos:
+            if lista_2[i] == " ":
+                lista_2[i] = "c"
+        elif i < aciertos + errores:
+            if lista_2[i] == " ":
+                lista_2[i] = "e"
         else:
-            lista_2.append("e")
+            if lista_2[i] == " ":
+                lista_2[i] = " "
     return lista_2
 
+def mostrar_tablero_3(aciertos, errores, posicion, lista, lista_2):
+    print(f"[{lista[0]}][{lista[1]}][{lista[2]}]")
+    print(f"[{lista_2[0]}][{lista_2[1]}][{lista_2[2]}]")
+    print(" " * (posicion * 3 + 1) + "^")
+    print(f"Aciertos: {aciertos}")
+    print(f"Errores: {errores}")
 
-def mostrar_tablero_3 (acirtos, errores):
-    print(f"""
-aciertos: {aciertos}
-errores: {errores}""")
+def ingresar_palabra():
+    ingreso_usuario = input("Ingresa palabra: ").lower()
+    while not ingreso_usuario.isalpha():
+        print("Ingrese solo letras.")
+        ingreso_usuario = input("Ingrese palabra: ").lower()
+    return ingreso_usuario
 
-def turno_letra (dicc, longitud, definicion):
-    for clave, valor in dicc.items():
-        letra = clave[0]
-        print(f"turno letra: {letra}   longitud palabra: {longitud}")
-        print(f"definicion: {definicion}")
-        return letra, definicion, longitud
-#turno_letra(dicc)
-
-def palabra_ingresada (palabra):
-    palabra = input()
-    return palabra
+def puntaje(aciertos):
+    return aciertos
 
 def pasapalabra():
+    puntaje_total = 0 
     aciertos = 0
     errores = 0
-    lista_2 = [" "," "," "]
+    lista_2 = [" ", " ", " "]
+    posicion = 0
+    lista_palabras_ingresadas = [] # nueva variable para almacenar las palabras ingresadas por el usuario
+
     for clave, valor in dicc.items():
         palabra = clave
-        print(palabra)
         longitud = len(palabra)
         definicion = valor
         letra = clave[0]
-        lista = [" "," "," "]
+        lista = [" ", " ", " "]
+
         for letra_1, clave_1 in enumerate(dicc.keys()):
             lista[letra_1] = clave_1[0]
-        print(f"""[{lista[0]}][{lista[1]}][{lista[2]}]""")
-        print(f"[{lista_2[0]}][{lista_2[1]}][{lista_2[2]}]")
-        print(f"""
-aciertos: {aciertos}
-errores: {errores}""")
-        print(f"turno letra: {letra}   longitud palabra: {longitud}")
-        print(f"definicion: {definicion}")
-        palabra = input()
-        if palabra == clave:
-            aciertos = aciertos + 1
-            for i in range(len(lista_2)):
-                if lista_2[i] == " ":
-                    lista_2[0] = "a"
-            #ista_2.append("a")
-            #print("aciertos:", aciertos)
-        elif palabra != clave :
-            errores = errores + 1
-            for i in range(len(lista_2)):
-                if lista_2[i] == " ":
-                    lista_2[0] = "e"
-            #lista_2.append("e")
-            #print("errores:", errores)
-        #print(lista_2)
-    return aciertos, errores
+
+        mostrar_tablero_3(aciertos, errores, posicion, lista, lista_2)
+        print(f"Turno letra: {letra} Longitud palabra: {longitud}")
+        print(f"Definicion: {definicion}")
+
+        if posicion == 0:
+            palabra_ingresada_1 = ingresar_palabra() # Cambio de nombre para evitar conflicto con la función
+            lista_palabras_ingresadas.append(palabra_ingresada_1) # agregar la palabra ingresada a la lista de palabras ingresadas
+            if palabra_ingresada_1 == clave:
+                aciertos += 1
+                lista_2[posicion] = "a"
+                posicion += 1
+                print(f"Turno letra: {letra} - Palabra de {longitud} - {palabra_ingresada_1} - acierto")
+            else:
+                errores += 1
+                lista_2[posicion] = "e"
+                posicion += 1
+                print(f"Turno letra: {letra} - Palabra de {longitud} - {palabra_ingresada_1} - error - Palabra correcta: {clave}")
+        else:
+            palabra_ingresada_2 = ingresar_palabra() # Cambio de nombre para evitar conflicto con la función
+            lista_palabras_ingresadas.append(palabra_ingresada_2) # agregar la palabra ingresada a la lista de palabras ingresadas
+            if palabra_ingresada_2 == clave:
+                aciertos += 1
+                lista_2[posicion] = "a"
+                posicion += 1
+                print(f"Turno letra: {letra} - Palabra de {longitud} - {palabra_ingresada_2} - acierto")
+            else:
+                errores += 1
+                lista_2[posicion] = "e"
+                posicion += 1
+                print(f"Turno letra: {letra} - Palabra de {longitud} - {palabra_ingresada_2} - error")
+
+    # Resumen de la partida
+    print("\n--- Resumen de la Partida ---")
+    print("-" * 50)
+    for i, (clave, valor) in enumerate(dicc.items()):
+        letra = clave[0]
+        longitud = len(clave)
+        palabra_correcta = clave
+        palabra_ingresada = lista_palabras_ingresadas[i] # nueva variable para almacenar la palabra ingresada por el usuario
+        resultado = "acierto" if lista_2[i] == "a" else "error"
+        if resultado == "error":
+            print(f"Turno de la letra: {letra} - Palabra de {longitud} letras - {palabra_ingresada} - {resultado} - Palabra correcta es : {palabra_correcta}")
+        else:
+            print(f"Turno de la letra: {letra} - Palabra de {longitud} letras - {palabra_ingresada} - {resultado}")
+    print("-" * 50)
+
+    puntaje_total = puntaje(aciertos)
+    print(f"\nPuntaje total: {puntaje_total}")
 
 pasapalabra()
